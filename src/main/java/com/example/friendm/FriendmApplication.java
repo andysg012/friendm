b6494b3554591770f3cfc6768ca65b476ec66566
sentaro <sentaro@sentaro-MacBook-Air.local>
2018-02-27 16:49:26 +0800

    first commit from spring initializr

---
+++ src/main/java/com/example/friendm/FriendmApplication.java
@@ -0,0 +1,12 @@
+package com.example.friendm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendmApplication.class, args);
+	}
+}
